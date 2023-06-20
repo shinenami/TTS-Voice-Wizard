4c50f908a1140fa5bc0c604362871c55cfcf9465
VRCWizard <101527472+VRCWizard@users.noreply.github.com>
2023-06-20 13:17:26 -0500

    Some early code snippets as I work on refactoring

---
+++ OSCVRCWiz/TextToSpeechWIP/AmazonPollyTTSModel.cs
@@ -0,0 +1,108 @@
+﻿using Amazon;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using OSCVRCWiz.Resources;
+using OSCVRCWiz.Settings;
+using OSCVRCWiz.Text;
+using Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
+using static OSCVRCWiz.TextToSpeech.TextToSpeechMVC;
+
+namespace OSCVRCWiz.TextToSpeech
+{
+    public class AmazonPollyTTSModel : TTSModel
+    {
+        // Additional properties specific to Amazon Polly
+
+        public virtual void Speak(TTSMessageQueue.TTSMessage message, CancellationToken ct)
+        {
+            // Implementation logic specific to Amazon Polly TTS service
+            // Perform the text-to-speech conversion using Amazon Polly API
+            Console.WriteLine($"Using Amazon Polly TTS service: {message.text}");
+            // Add the code to call the Amazon Polly API and convert the text to speech
+
+            var pollyService = new AmazonPollyService();
+            pollyService.SpeakAsync(message, ct);
+        }
+    }
+
+    public class AmazonPollyService
+    {
+
+        private readonly AmazonPollyClient _client;
+
+        public AmazonPollyService()
+        {
+            var AWSaccessKeyId = Settings1.Default.yourAWSKey;
+            var AWSsecretKey = Settings1.Default.yourAWSSecret;
+            var AWSRegion = RegionEndpoint.GetBySystemName(Settings1.Default.yourAWSRegion);
+
+            _client = new AmazonPollyClient(AWSaccessKeyId, AWSsecretKey, AWSRegion);
+
+        }
+        public async Task SpeakAsync(TTSMessageQueue.TTSMessage message, CancellationToken ct)
+        {
+            try
+            {
+                var response = await SynthesizeSpeechAsync(message);
+                await PlaySpeechAsync(response.AudioStream, message,ct);
+                response.Dispose();
+            }
+            catch (Exception ex)
+            {
+                OutputText.outputLog("[Amazon Polly TTS Error: " + ex.Message + "]", Color.Red);
+                TTSMessageQueue.PlayNextInQueue();
+
+            }
+            
+        }
+        private async Task<SynthesizeSpeechResponse> SynthesizeSpeechAsync(TTSMessageQueue.TTSMessage message)
+        {
+            var request = new SynthesizeSpeechRequest
+            {
+                Text = message.text,
+                // Other request parameters
+            };
+
+            return await _client.SynthesizeSpeechAsync(request);
+        }
+
+        private async Task PlaySpeechAsync(Stream audioStream,TTSMessageQueue.TTSMessage message, CancellationToken ct)
+        {
+            using (audioStream)
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                await WriteSpeechToStream(audioStream, memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                AudioDevices.playSSMLMP3Stream(memoryStream, message, ct);//Next refactor how audio is played
+                memoryStream.Dispose();
+            }
+        }
+        public async Task WriteSpeechToStream(Stream audioStream, MemoryStream output)
+        {
+
+            byte[] buffer = new byte[2 * 1024];
+            int readBytes;
+
+            while ((readBytes = audioStream.Read(buffer, 0, 2 * 1024)) > 0)
+            {
+                output.Write(buffer, 0, readBytes);
+            }
+
+            // Flushes the buffer to avoid losing the last second or so of
+            // the synthesized text.
+            output.Flush();
+
+        }
+
+
+
+
+    }
+}
---
+++ OSCVRCWiz/TextToSpeechWIP/TextToSpeechMVC.cs
@@ -0,0 +1,115 @@
+﻿using OSCVRCWiz.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCVRCWiz.TextToSpeech
+{
+    public class TextToSpeechMVC
+    {
+        public class TTSModel
+        {
+            public string ServiceName { get; set; }
+            // Additional properties for the TTS service
+
+            public void Speak(TTSMessageQueue.TTSMessage message, CancellationToken ct)
+            {
+                // Implementation logic to call the respective TTS API service based on ServiceName
+                // Perform the text-to-speech conversion
+                Console.WriteLine($"Using {ServiceName} TTS service: {message.text}");
+            }
+        }
+
+        public class TTSView
+        {
+            public TTSMessageQueue.TTSMessage GetInput(string InputMode, string text)
+            {
+                // message to convert to speech;
+
+                TTSMessageQueue.TTSMessage message = new TTSMessageQueue.TTSMessage();
+                VoiceWizardWindow.MainFormGlobal.Invoke((MethodInvoker)delegate ()
+                {
+                    message.text = text;
+                    message.TTSMode = VoiceWizardWindow.MainFormGlobal.comboBoxTTSMode.Text;
+                    message.Voice = VoiceWizardWindow.MainFormGlobal.comboBox2.Text; //comboBox2
+                    message.Accent = VoiceWizardWindow.MainFormGlobal.comboBox5.Text; //comboBox5
+                    message.Style = VoiceWizardWindow.MainFormGlobal.comboBox1.Text;  //comboBox1
+                    message.Pitch = VoiceWizardWindow.MainFormGlobal.trackBarPitch.Value;
+                    message.Speed = VoiceWizardWindow.MainFormGlobal.trackBarSpeed.Value;
+                    message.Volume = VoiceWizardWindow.MainFormGlobal.trackBarVolume.Value;
+                    message.SpokenLang = VoiceWizardWindow.MainFormGlobal.comboBox4.Text;  //comboBox4
+                    message.TranslateLang = VoiceWizardWindow.MainFormGlobal.comboBox3.Text;  //comboBox3
+                    message.STTMode = InputMode;
+                    message.AzureTranslateText = "[ERROR]";
+                });
+                return message;
+            }
+
+            public void DisplayOutput(string output)
+            {
+                Console.WriteLine($"Speech output: {output}");
+            }
+        }
+
+        public class TTSController
+        {
+            public List<TTSModel> ttsModels;
+            public TTSView ttsView;
+
+            public TTSController()
+            {
+                ttsModels = new List<TTSModel>();
+                ttsView = new TTSView();
+            }
+
+            public void AddTTSModel(TTSModel model)
+            {
+                ttsModels.Add(model);
+            }
+
+            public void SpeakText(TTSMessageQueue.TTSMessage message, CancellationToken ct)
+            {
+                foreach (var model in ttsModels)
+                {
+                    model.Speak(message, ct);
+                }
+            }
+
+            public void DisplayOutput(string output)
+            {
+                ttsView.DisplayOutput(output);
+            }
+        }
+
+        public class ExampleUsage
+        {
+            static void Example()
+            {
+                TTSController ttsController = new TTSController();
+
+                // Add TTS models for different services
+                // ttsController.AddTTSModel(new TTSModel { ServiceName = "System Speech" }); //able to test multiple models at once if needed
+                ttsController.AddTTSModel(new AmazonPollyTTSModel());
+
+
+                CancellationTokenSource speechCt = new();
+                // Get input from the user
+                TTSMessageQueue.TTSMessage input = ttsController.ttsView.GetInput("Amazon Polly", "here are the words I am speaking");
+
+                // Speak the input using all TTS models
+                ttsController.SpeakText(input, speechCt.Token);
+
+
+                // If I want to cancel the speak for what ever reason
+                // speechCt.Cancel();
+
+
+         
+            }
+        }
+
+
+    }
+}
